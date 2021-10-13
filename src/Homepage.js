1773366611e001fa03a279d4e197cb5092c561d6
Serena <serenaqkong@gmail.com>
2021-10-12 17:35:15 -0700

    created homepage component

---
+++ src/Homepage.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+/** Homepage
+ * 
+ * Routes -> Homepage
+ */
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <h1>homepage</h1>
+    </div>
+  )
+}
+
+export default Homepage;
