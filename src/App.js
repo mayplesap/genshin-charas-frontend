d0577560716747d4421624e97378e712a5159626
Serena <serenaqkong@gmail.com>
2021-10-12 17:36:14 -0700

    added background image

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
+import Routes from "./Routes";
+import background1 from "./img/background1.jpeg";
 import { getChara } from "./Api";
 
 function App() {
   const [charas, setCharas] = useState([]);
+  const [background, setBackground] = useState(background1);
 
   useEffect(function() {
     async function getAllCharas() {
       const list = await getChara();
       setCharas(list);
+      setBackground(background1);
     }
 
     getAllCharas();
   }, []);
 
+
   return (
-    <div className="App">
-      
+    <div className="App" style={{backgroundImage: `url(${background})`}}>
+      <Routes />
     </div>
   );
 }
