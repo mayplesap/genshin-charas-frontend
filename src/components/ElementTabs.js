5a560fa6dd9174b133c48b6812478d6a6ee886ac
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-14 23:15:43 -0700

    renders element buttons

---
+++ src/components/ElementTabs.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import AnemoIcon from "../img/anemo.png"
+import PyroIcon from "../img/pyro.png"
+
+/**TODO: comments */
+function ElementTabs() {
+
+  return (
+    <div>
+      <img src={ PyroIcon } alt="Pyro" className="btn" width="100px" style={{backgroundColor: "transparent"}}/>
+    </div>
+  );
+}
+  
+export default ElementTabs;
