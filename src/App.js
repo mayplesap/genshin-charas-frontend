3e0da85cd150786a63f037808be7da4882ab3de0
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-14 23:13:57 -0700

    added isLoading

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import './App.css';
+import './styles/App.css';
 import React, { useState, useEffect } from "react";
 import Routes from "./Routes";
-import Logo from "./Logo";
+import Logo from "./components/Logo";
 import background1 from "./img/background1.jpeg";
 import { getChara } from "./Api";
 
 function App() {
   const [charas, setCharas] = useState([]);
   const [background, setBackground] = useState(background1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(function() {
     async function getAllCharas() {
       const list = await getChara();
       setCharas(list);
       setBackground(background1);
+      setIsLoading(false);
     }
-
     getAllCharas();
   }, []);
 
+  if(isLoading) return <div>...Loading</div>
 
   return (
     <div className="App" style={{backgroundImage: `url(${background})`}}>
       <Logo />
-      <Routes />
+      <Routes charas={charas} />
     </div>
   );
 }
